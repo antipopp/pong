1f73fe59388bdb03b26883d8c2cc47b1da7d55b2
Francesco Cartier <francesco@linux-3r9j.suse>
2018-03-16 18:07:21 +0100

    fixed login and registration, again..
    Committer: Francesco Cartier <francescocartier@gmail.com>

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
-<?php
-	include("auth.php");
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="it">
 	<head>
-		<meta charset="utf-8">
+		<meta charset="utf-8"> 
+		<meta name = "author" content = "Francesco Cartier">
+		<meta name = "keywords" content = "index">
+		<link rel="stylesheet" href="/css/style.css" type="text/css">		
 		<title>PONG!</title>
-		<link rel="stylesheet" href="css/style.css" />
 	</head>
+	<body>
 	<body>
 		<header><h1>PONG</h1></header>
 		<nav>
 			<ul>
-				<li><a href="logout.php" class="retroButton">Logout</a></li>
+				<?php
+					session_start();
+					if(!isset($_SESSION["userId"])) {
+						echo '<li><a href="login.php" class="retroButton">Login</a></li>';
+						echo '<li><a href="registrazione.php" class="retroButton">Registrazione</a></li>';
+					}
+					else {
+						echo '<li><a href="logout.php" class="retroButton">Logout</a></li>';
+					}
+				?>
 				<li><a href="#" class="retroButton">Classifica</a></li>
 			</ul>
 		</nav>
