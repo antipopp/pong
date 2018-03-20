8b5bbbdf165940ecaaf846cc391ceea259c10d66
Francesco Cartier <francesco@linux-3r9j.suse>
2018-03-20 11:55:12 +0100

    added sql dump

--- pong.sql
+++ pong.sql
@@ -34,20 +34,6 @@ CREATE TABLE `leaderboard` (
   `lost` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `leaderboard`
---
-
-INSERT INTO `leaderboard` (`id`, `win`, `lost`) VALUES
-(5, 0, 0),
-(6, 0, 0),
-(7, 0, 0),
-(8, 0, 0),
-(9, 0, 0),
-(10, 0, 0),
-(11, 0, 0),
-(12, 0, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -62,18 +48,6 @@ CREATE TABLE `users` (
   `trn_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `trn_date`) VALUES
-(1, 'ciccio', 'email@falsa.it', 'ce2505c0da3616108de4736804aff6b5', '2018-03-12 15:55:43'),
-(7, 'ciccio', 'sultano@gmail.it', 'e165d4f2174b66a7d1a95cb204d296eb', '2018-03-16 11:03:59'),
-(8, 'ciccio', 'email2@falsa.it', 'ffe7470430a737c4ce6dc74bea0155d5', '2018-03-16 12:49:58'),
-(9, 'franco', 'franco@email.it', '6dd1411a66159040b7fff30d0097dbe4', '2018-03-16 16:41:23'),
-(10, 'francos', 'francos@gmail.com', 'f1cb3645724a42ea0e1fab16e3532e9e', '2018-03-16 16:42:48'),
-(11, 'franco', 'frnaco@email.it', '6dd1411a66159040b7fff30d0097dbe4', '2018-03-20 09:14:43'),
-(12, 'franci', 'franci@ema.ir', '17166193b35a231d8031c52931e06a70', '2018-03-20 09:19:18');
 
 --
 -- Triggers `users`
