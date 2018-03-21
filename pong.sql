2a1abf3e2c4196859d413e16e8613787a637b66d
Francesco Cartier <francesco@linux-3r9j.suse>
2018-03-21 18:26:39 +0100

    updated db structure

--- pong.sql
+++ pong.sql
@@ -10,59 +10,59 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pong`
+-- Database: pong
 --
-CREATE DATABASE IF NOT EXISTS `pong` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `pong`;
+CREATE DATABASE IF NOT EXISTS pong DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE pong;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `leaderboard`
+-- Table structure for table leaderboard
 --
 
-CREATE TABLE `leaderboard` (
-  `user` varchar(50) NOT NULL,
-  `win` int(11) NOT NULL,
-  `lost` int(11) NOT NULL
+CREATE TABLE leaderboard (
+  user varchar(50) NOT NULL,
+  win int(11) NOT NULL,
+  lost int(11) NOT NULL,
+  ratio decimal(2,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table users
 --
 
-CREATE TABLE `users` (
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `trn_date` datetime NOT NULL
+CREATE TABLE users (
+  username varchar(50) NOT NULL,
+  email varchar(50) NOT NULL,
+  password varchar(50) NOT NULL,
+  trn_date datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Triggers `users`
+-- Triggers users
 --
 DELIMITER $$
-CREATE TRIGGER `leaderboard_init` AFTER INSERT ON `users` FOR EACH ROW INSERT INTO leaderboard (user, win, lost) VALUES (NEW.username, 0, 0)
+CREATE TRIGGER `leaderboard_init` AFTER INSERT ON `users` FOR EACH ROW INSERT INTO leaderboard (user, win, lost, ratio) VALUES (NEW.username, 0, 0, 0)
 $$
 DELIMITER ;
 
 --
--- Indexes for table `leaderboard`
+-- Indexes for table leaderboard
 --
+ALTER TABLE leaderboard
+  ADD PRIMARY KEY (user),
+  ADD UNIQUE KEY username (user);
 
-ALTER TABLE `leaderboard`
-  ADD PRIMARY KEY (`user`),
-  ADD UNIQUE KEY `username` (`user`);
-
 --
--- Indexes for table `users`
+-- Indexes for table users
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`username`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE users
+  ADD PRIMARY KEY (username),
+  ADD UNIQUE KEY username (username),
+  ADD UNIQUE KEY email (email);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
