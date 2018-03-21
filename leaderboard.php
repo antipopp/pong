aba75032d39a6787c64f3d301ff1aba3e1589b50
Francesco Cartier <francesco@linux-3r9j.suse>
2018-03-21 18:21:21 +0100

    fixed leaderboard structure

--- leaderboard.php
+++ leaderboard.php
@@ -1,34 +1,55 @@
-<html>
-    <head>
-        <title>Last 10 Results</title>
-    </head>
-    <body>
-        <table>
-        <thead>
-            <tr>
-                <td>User</td>
-                <td>Vittorie</td>
-                <td>Sconfitte</td>
-                <td>Rapporto</td>
-            </tr>
-        </thead>
-        <tbody>
-        <?php
-            require_once 'db.php';
-            $results = mysqli_query($con,"SELECT * FROM leaderboard ORDER BY ratio DESC");
-            while($row = mysqli_fetch_assoc($results)) {
-            ?>
-                <tr>
-                    <td><?php echo $row['user']?></td>
-                    <td><?php echo $row['win']?></td>
-                    <td><?php echo $row['lost']?></td>
-                    <td><?php echo $row['ratio']?></td>
-                </tr>
+<!DOCTYPE html>
+<html lang="it">
+	<head>
+		<meta charset="utf-8"> 
+		<meta name = "author" content = "Francesco Cartier">
+		<meta name = "keywords" content = "classifica">
+		<link rel="stylesheet" href="/css/style.css" type="text/css">       
+		<title>PONG!</title>
+	</head>
+	<body>
+	<body>
+		<header><h1><a href="index.php">PONG</a></h1></header>
+		<nav>
+			<ul>
+				<?php
+					session_start();
+					if(!isset($_SESSION["username"])) {
+						echo '<li><a href="login.php" class="retroButton">Login</a></li>';
+						echo '<li><a href="registration.php" class="retroButton">Registrazione</a></li>';
+					}
+					else {
+						echo '<li><a href="logout.php" class="retroButton">Logout</a></li>';
+					}
+				?>
+			</ul>
+		</nav>
+		<table>
+		<thead>
+			<tr>
+				<td>User</td>
+				<td>Vittorie</td>
+				<td>Sconfitte</td>
+				<td>Rapporto</td>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+			require_once 'db.php';
+			$results = mysqli_query($con,"SELECT * FROM leaderboard ORDER BY ratio DESC");
+			while($row = mysqli_fetch_assoc($results)) {
+			?>
+				<tr>
+					<td><?php echo $row['user']?></td>
+					<td><?php echo $row['win']?></td>
+					<td><?php echo $row['lost']?></td>
+					<td><?php echo $row['ratio']?></td>
+				</tr>
 
-            <?php
-            }
-            ?>
-            </tbody>
-            </table>
-    </body>
+			<?php
+			}
+			?>
+			</tbody>
+			</table>
+	</body>
 </html>
